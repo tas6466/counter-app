867212fb4890b8cc72047a2c9c114901aeb672ac
Tylar Sanderson <tas6466@psu.edu>
2024-10-08 00:48:38 -0400

    code

--- src/counter-app.js
+++ src/counter-app.js
@@ -10,49 +10,90 @@ export class counterApp extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.title = "";
+    this.counter = 0;
+    this.min = 0;
+    this.max = 25;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      counter: { type: Number },
+      min: { type: Number },
+      max: { type: Number }
     };
   }
 
   static get styles() {
-    return [super.styles,
-    css`
+    return css`
       :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+        display: inline-block;
+        width: 150px;
+        margin: 16px auto;
+        text-align: center;
+        padding: 16px;
+        border: 2px solid #CCCCCC;
+        border-radius: 8px;
+        background-color: #FFFFFF;
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+      .counter {
+        font-size: 96px;
+        margin-bottom: 4px;
       }
-      div {
-        padding: 0;
-        margin: 0;
+      .button-container {
+        display: flex;
+        justify-content: center;
+        gap: 8px;
       }
-    `];
+      button {
+        padding: 4px 8px;
+        font-size: 16px;
+        border: 2px solid transparent;
+        border-radius: 4px;
+        background-color: cornflowerblue;
+        color: white;
+      }
+      button:hover, button:focus {
+        background-color: #FFFFFF;
+        color: cornflowerblue;
+        border: 2px solid #000000;
+        cursor: pointer;
+      }
+    `;
   }
 
   render() {
+    const colorStyle = this.getColor();
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
+      <div>
+        Count:
+          <div class="counter" style="${colorStyle}">
+            ${this.counter}
+          </div>
+          <div class="button-container">
+            <button @click=${this.increment} ?disabled="${this.max === this.counter}">+</button>
+            <button @click=${this.decrement} ?disabled="${this.min === this.counter}">-</button>
+          </div>
+      </div>`;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+  increment() {
+    if (this.counter < this.max) {
+      this.counter += 1;
+    }
+  }
+
+  decrement() {
+    if (this.counter > this.min) {
+      this.counter -=1;
+    }
+  }
+
+  getColor() {
+    if (this.counter >= this.max || this.counter <= this.min) return 'color: blue;';
+    if (this.counter === 18) return 'color: green;';
+    if (this.counter === 21) return 'color: purple;';
+    return '';
   }
 }
 
